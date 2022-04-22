168370451f57de6ec6b3bf10704e2a78c8f0a2c2
guss-renteria <gusstavo.renteria.g@gmail.com>
2022-04-22 02:30:51 +0000

    se añadio 'Modal' para agregar tareas

--- src/views/Tasks/Tasks.jsx
+++ src/views/Tasks/Tasks.jsx
@@ -1,18 +1,51 @@
-import { useSelector } from 'react-redux';
+import { useRef } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addTask, delTask } from '../../actions/task.actions';
 
 import './tasks.scss';
 
+import AddTaskModal from '../../components/tasks/AddTaskModal';
+
 const Tasks = () => {
+  const add_task_modal = useRef(null);
+  const dispatch = useDispatch();
+
   const { tasks } = useSelector(state => state.tasks);
+
+  const addNewTask = e => {
+    e.preventDefault();
+
+    let data = Object.fromEntries(new FormData(e.target));
+    data = {
+      ...data,
+      next: null,
+      tags: data.tags ? data.tags.split(',') : [],
+    }
+
+    dispatch(addTask(data));
+    e.target.reset();
+
+    add_task_modal?.current?.classList.remove('active');
+  }
+
+  const removeTask = id => {
+    dispatch(delTask(id));
+  }
+
+  const openCreateTask = e => {
+    e.preventDefault();
+    add_task_modal?.current?.classList.add('active');
+  }
 
   // * return TASKS
   return (
+    <>
     <div className='tasks'>
       <div className='task-box'>
         <div className='task-box__header'>
           <h1 className='title'>Tareas</h1>
-          <p className='length'>{tasks && tasks.length}</p>
-          <button className='add'>+</button>
+          <p className='length'>{ tasks && tasks.length }</p>
+          <button className='add' onClick={ openCreateTask }>+</button>
         </div>
         <div className='task-box__container'>
           { tasks?.length > 0 ? tasks.map((task, key) => (
@@ -33,7 +66,7 @@ const Tasks = () => {
               </div>
               <div className='task-delete'>
                 <button className='update-button'>Actualizar</button>
-                <button className='delete-button'>Eliminar</button>
+                <button className='delete-button' onClick={ () => removeTask(task.id) }>Eliminar</button>
               </div>
             </div>
           )) : (
@@ -42,6 +75,8 @@ const Tasks = () => {
         </div>
       </div>
     </div>
+    <AddTaskModal addTask={ addNewTask } reference={ add_task_modal }/>
+    </>
   )
 }
 
